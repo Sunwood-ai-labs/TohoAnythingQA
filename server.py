a0d15cd041b9e07d77baad854aa41ddb82dde1ae
Maki <sunwood.ai.labs@gmail.com>
2024-08-28 17:54:28 +0900

    ✨ [feat] グラフ可視化ページのURLを修正
    - `server.py`のグラフ可視化ページのURLを`index3.html`から`visual/index_neo4j.html`に修正しました。
    - これにより、ユーザーは正しいURLでグラフ可視化ページにアクセスできるようになります。

--- server.py
+++ server.py
@@ -7,5 +7,5 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
-    print(f"Open http://localhost:{PORT}/index3.html in your browser")
+    print(f"Open http://localhost:{PORT}/visual/index_neo4j.html in your browser")
     httpd.serve_forever()
